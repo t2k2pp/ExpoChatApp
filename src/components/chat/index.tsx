88fd792927a4d1cd1e6747e909ad1ee403b698d9
tera <osia321@hotmail.com>
2025-12-03 00:45:12 +0900

    fix: remove duplicate FeatureSelectionModal export

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -6,5 +6,4 @@ export { MessageBubble } from './MessageBubble';
 export { ChatInput } from './ChatInput';
 export { TypingIndicator } from './TypingIndicator';
 export { FeatureSelectionModal } from './FeatureSelectionModal';
-export { FeatureSelectionModal } from './FeatureSelectionModal';
 export { ThinkingModal } from './ThinkingModal';
